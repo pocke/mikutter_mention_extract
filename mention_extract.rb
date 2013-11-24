84ee6d0e6a0912c65f75eb9a88e14fd6c0f733d0
pocke <p.ck.t22@gmail.com>
2013-11-24 19:51:12 +0900

    refactor

--- mention_extract.rb
+++ mention_extract.rb
@@ -5,16 +5,12 @@
 # http://opensource.org/licenses/mit-license.php
 
 Plugin.create(:mention_extract) do
-
   settings('メンション抽出') do
     input '抽出する正規表現', :mention_extract_pattern
   end
 
-  on_update do | service, msgs |
-    pattern = UserConfig[:mention_extract_pattern]
-    msgs = msgs.select{ |m| /#{pattern}/ =~ m[:message] }
-    #timeline(:mentions) << msgs
-    Plugin.call(:mention, service, msgs) if msgs != []
+  on_update do |service, msgs|
+    pattern ||= Regexp.new UserConfig[:mention_extract_pattern]
+    Plugin.call :mention, service, msgs.select{|m| pattern =~ m[:message] }
   end
-
 end
